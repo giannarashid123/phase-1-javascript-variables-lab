0891cc956d1431a8553677ec56fbf9d012b69ddc
mariamrashid <giannarashid@gmail.com>
2025-06-05 12:13:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber'; // Cannot be reassigned, as required
+
+let mostProfitableNeighborhood = 'Chelsea'; // Can be reassigned
+
+let companyCeo = 'Susan Smith'; // Can be reassigned
